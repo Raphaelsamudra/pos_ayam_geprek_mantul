b05956d5c91480a71ab4482001724bd4447b4374
Raphaelsamudra <raphael.samudra@gmail.com>
2024-11-05 23:07:35 +0700

    .

--- _sidebar.php
+++ _sidebar.php
@@ -25,7 +25,7 @@
             <a href="bo" class="nav-link">
               <i class="nav-icon fa fa-desktop"></i> 
               <p>
-                 Dashboardd
+                 Dashboard
               </p>
             </a>
           </li>
